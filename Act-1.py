a3b8df31035677a6a946ad52372277cf24236302
Mayra30345 <palmayra966@gmail.com>
2025-10-17 16:33:45 +0530

    Add files via upload

---
+++ Act-1.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((400,300))
+done=False
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done=True
+    pygame.draw.rect(screen,(200,90,30),pygame.Rect(30,0,60,40))
+    pygame.display.flip()
